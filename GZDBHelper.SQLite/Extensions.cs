c89ae0bbdc21db29b502dae4da7a436d5e30d734
Garson <garson_zhang@163.com>
2022-02-06 17:13:03 +0800

    1

---
+++ GZDBHelper.SQLite/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using GZDBHelper.SQLite.Model;
+namespace GZDBHelper
+{
+    /// <summary>
+    /// 扩展
+    /// </summary>
+    public static class Extensions
+    {
+
+
+        /// <summary>
+        /// 获得 MSSQL 基本工具
+        /// </summary>
+        /// <param name="current"></param>
+        /// <returns></returns>
+        public static SQLiteTools GetSQLiteTools(this IDatabase current)
+        {
+            return new SQLiteTools(current);
+        }
+
+    }
+}
